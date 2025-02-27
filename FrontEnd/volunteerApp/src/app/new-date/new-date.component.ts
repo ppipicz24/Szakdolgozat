ac0adcde19b4b85a62434801d3de020702005f59
Pipicz Petra <ppipicz24@gmail.com>
2025-02-27 16:20:07 +0100

    router

--- FrontEnd/volunteerApp/src/app/new-date/new-date.component.ts
+++ FrontEnd/volunteerApp/src/app/new-date/new-date.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-date',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-date.component.html',
   styleUrl: './new-date.component.css'
 })
