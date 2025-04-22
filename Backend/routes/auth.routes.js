81a306e4c8075d8ce55b4fbca01da946a0ea290e
Pipicz Petra <ppipicz24@gmail.com>
2025-04-22 20:11:26 +0200

    auth response

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -57,7 +57,7 @@ router.post("/register", async (req, res) => {
       createdAt: Date.now(),
     });
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(200).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
